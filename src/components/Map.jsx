1f13dbaf2e0ecb2853050790d8a431cc0386cf01
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-18 21:02:44 +0530

    Integrating map into weather app

---
+++ src/components/Map.jsx
@@ -0,0 +1,20 @@
+import {  useSelector } from "react-redux"
+
+
+export function Map() {
+
+    const {city} = useSelector((store)=>store.credential)
+
+    return  <iframe
+    title="weather"
+   width="500"
+    height="340"
+    id="gmap_canvas"
+    src={`https://maps.google.com/maps?q=${city}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+   frameBorder="0"
+   scrolling="no"
+  marginHeight="0"
+ marginWidth="0"
+></iframe>
+
+}
