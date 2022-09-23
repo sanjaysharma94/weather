3e15aba4b6827faf1e0e67d3e585617a321b65d6
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-23 21:06:04 +0530

    creating sunrise sunset graph

---
+++ src/components/SunRiseSet.jsx
@@ -0,0 +1,90 @@
+import {  useSelector } from "react-redux"
+import { DateTime } from "luxon";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+
+export const options = {
+  maintainAspectRatio: true,
+  
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: 'sunrise sunset',
+    },
+  },
+};
+
+
+
+
+
+
+export function SunRise() {
+
+  let time1 = (tm)=>{
+
+  
+    return (DateTime.fromSeconds(tm).toFormat("hh"))
+  
+  };
+
+  let time2 = (tm)=>{
+
+  
+    return (DateTime.fromSeconds(tm).toFormat("hh:mm a"))
+  
+  };
+  
+
+  const  currentdata = useSelector((store)=>store.credential.current)
+
+ const labels = ["sunrise" , "sunset"]
+ const data = {
+    labels ,
+   datasets: [
+    {
+      fill: true,
+       label: "sunrise sunset",
+       data:currentdata.map((e)=>time1(e)),
+       borderColor: 'rgb(235, 156, 53)',
+       backgroundColor: 'rgba(235, 177, 53, 0.959)',
+    },
+  ],
+ };
+
+  return  <div style={{ margin:"auto", height:"400px", width:"800px" ,marginTop:"10px", marginBottom:"170px"}} >
+    <div style={{display:"flex", justifyContent:"space-between" }}>
+    <div><h1>Sunrise</h1>{ currentdata[0] && <h1>{time2(currentdata[0])}</h1>}</div>
+    <div><h1>Sunset</h1>{currentdata[1] && <h1 >{time2(currentdata[1])}</h1>}</div>
+    </div>
+    {<Line options={options} data={data} />} ;
+      </div> 
+ }
