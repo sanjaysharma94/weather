f28e773bcde015f0a064ac0ff9b53b57926302b6
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-09-24 22:22:40 +0530

    formatting

--- src/components/Corousel.jsx
+++ src/components/Corousel.jsx
@@ -9,19 +9,12 @@ import sun from "../IconFiles/sun.png"
 
 
 export const Slider = () => {
- 
-
-const  data = useSelector((store)=>store.credential.forecast)
-         
+const  data = useSelector((store)=>store.credential.forecast)        
 let time = (tm)=>{
-
-  return DateTime.fromSeconds(tm).toFormat("ccc")
+ return DateTime.fromSeconds(tm).toFormat("ccc")
 
 };
-
-
-
-    
+ 
   return (
     <div className='scroll' style={{  margin:"auto",  display:"flex", gap:"20px", flexFlow:'nowrap', width:"50%",overflowX:"scroll",scrollbarColor: "rebeccapurple" }} >
       
@@ -36,18 +29,19 @@ let time = (tm)=>{
             }}
           >
             <p style={{ marginTop: 0.5, marginBottom: 0.5 }}>{time(e.dt)}</p>
-           
             <p style={{ marginTop: 0.5, marginBottom: 0 }}>{e.temp.day}°C</p>
             <img 
             src= {e.weather[0].main==="Clear"?sun : e.weather[0].main==="Rain" ? rain :cloud}
-              style={{
+            style=
+            {
+              {
                 width: "80%",
                 height: "50%",
                 marginTop: 0,
                 marginBottom: 0
-              }}
-             
-              alt="ram"
+              }
+            }
+              alt="mausamType"
             />
             <h4 style={{ marginTop: 0 , marginBottom: 0 }}>{e.weather[0].main}</h4>
           </div>
